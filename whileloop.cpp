11804575b8f34e9f51a5a913eab9260c5151ccb0
AMAN RAJ <amantk2205@gmail.com>
2025-09-01 00:56:10 +0530

    Add files via upload

---
+++ whileloop.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>   // Includes all standard C++ libraries
+using namespace std;
+
+int main() {
+    int i = 0;                     // Initialize counter i with 0
+    while(i <= 10) {               // Loop runs while i is less than or equal to 10
+        cout << "\nHello World";   // Print "Hello World" on a new line
+        cout << "\n" << i;         // Print the current value of i
+        i = i + 1;                 // Increment i by 1
+    }
+    return 0;      // End of the program
+}
